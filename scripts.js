ddbed88bea17e9027f3de79daa6ff332bb8adc5e
Jonhycub <jonhycub@gmail.com>
2023-12-26 21:47:43 -0500

    Refactoring the code to display the results.
    Refactored the code to show results properly and reset the game after the score reaches 5.

--- scripts.js
+++ scripts.js
@@ -1,52 +1,72 @@
 // Scores.
 let playerScore = 0;
 let computerScore = 0;
+const myScore = document.querySelector('#player-score');
+const pcScore = document.querySelector('#computer-score');
+const roundResult = document.querySelector('#round-result');
 
-// Gets the random computer choice.
+//Function that resets the game
+function resetGame(){
+  roundResult.innerText ="First to score 5 points wins the game";
+  playerScore = 0;
+  computerScore = 0;
+}
+
+// Function that gets the random computer choice.
 function getComputerChoice(){
   let choices = ["rock", "paper", "scissors"];
   let random = Math.floor(Math.random() * choices.length);
   return choices[random];
 }
 
-// Plays a round and increases the score of the winner of the round by 1.
+//Function that plays a round and increases the score of the winner of the round by 1.
 function playRound(playerSelection, computerSelection){
   if(playerSelection == "rock"){
     if(computerSelection == "rock"){
       playerScore += 1;
       computerScore += 1;
-      return "It's a tie!";
+      roundResult.innerText ="It's a tie!";
     }else if(computerSelection == "scissors"){
       playerScore += 1;
-      return "You win! Rock beats Scissors.";
+      roundResult.innerText ="You win! Rock beats Scissors.";
     }else{
       computerScore += 1;
-      return "You lose! Paper beats Rock.";
+      roundResult.innerText ="You lose! Paper beats Rock.";
     }
   }else if(playerSelection == "paper"){
     if(computerSelection == "paper"){
       playerScore += 1;
       computerScore += 1;
-      return "It's a tie!";
+      roundResult.innerText ="It's a tie!";
     }else if(computerSelection == "rock"){
       playerScore += 1;
-      return "You win! Paper beats Rock.";
+      roundResult.innerText ="You win! Paper beats Rock.";
     }else{
       computerScore += 1;
-      return "You lose! Scissors beats Paper.";
+      roundResult.innerText ="You lose! Scissors beats Paper.";
     }
   }else{
     if(computerSelection == "scissors"){
       playerScore += 1;
       computerScore += 1;
-      return "It's a tie!";
+      roundResult.innerText ="It's a tie!";
     }else if(computerSelection == "paper"){
       playerScore += 1;
-      return "You win! Scissors beats Paper.";
+      roundResult.innerText ="You win! Scissors beats Paper.";
     }else{
       computerScore += 1;
-      return "You lose! Rock beats Scissors.";
+      roundResult.innerText ="You lose! Rock beats Scissors.";
     }
+  }
+  if(playerScore == 5 && computerScore == 5){
+    alert("It's a tie!");
+    resetGame();
+  }else if(playerScore == 5){
+    alert("You Win!");
+    resetGame();
+  }else if(computerScore == 5){
+    alert("You Lose!");
+    resetGame();
   }
 }
 
@@ -55,42 +75,25 @@ const buttonRock = document.querySelector('#rock');
 buttonRock.addEventListener('click', () =>{
     playerSelection = 'rock';
     computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
-  });
+    console.log(playRound(playerSelection, computerSelection));
+    myScore.innerText = "Player: " + playerScore;
+    pcScore.innerText = "Computer: " + computerScore;
+});
 
 const buttonPaper = document.querySelector('#paper');
 buttonPaper.addEventListener('click', () =>{
     playerSelection = 'paper';
     computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
-  });  
+    console.log(playRound(playerSelection, computerSelection));
+    myScore.innerText = "Player: " + playerScore;
+    pcScore.innerText = "Computer: " + computerScore;
+});  
 
 const buttonScissors = document.querySelector('#scissors');
 buttonScissors.addEventListener('click', () =>{
     playerSelection = 'scissors';
     computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
-  });
-
-
-// Main game loop that displays the score and the winner.
-// function game(){ 
-//   while(playerScore < 5 && computerScore < 5){
-//     const playerSelection = prompt("Enter your choice: ").toLowerCase();
-//     const computerSelection = getComputerChoice();
-//     console.log(playRound(playerSelection, computerSelection));
-
-//     console.log("Your score is: " + playerScore);
-//     console.log("The computer's score is: " + computerScore);
-//   }
-//   if(playerScore == 5 && computerScore == 5){
-//     alert("It's a tie!");
-//   }else if(playerScore == 5){
-//     alert("You win!");
-//   }else{
-//     alert("You lose!");
-//   }
-// }
-
-//Calling the game function.
-// game();
+    console.log(playRound(playerSelection, computerSelection));
+    myScore.innerText = "Player: " + playerScore;
+    pcScore.innerText = "Computer: " + computerScore;
+});
